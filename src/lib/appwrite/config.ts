8c385684faaf89b295e10b340f45acbefb1baf6a
Harsha Vardhan <94213293+harsha42703@users.noreply.github.com>
2023-11-17 14:33:22 +0530

    first commit

---
+++ src/lib/appwrite/config.ts
@@ -0,0 +1,21 @@
+import { Client, Account, Databases, Storage, Avatars } from "appwrite";
+
+export const appwriteConfig = {
+  url: import.meta.env.VITE_APPWRITE_URL || 'http://cloud.appwrite.io/v1',
+  projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID || '655714c6263c09084cd7',
+  databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID || '65571ab61e4d1561f6c5',
+  storageId: import.meta.env.VITE_APPWRITE_STORAGE_ID || '65571a8262ae005f0bb2',
+  userCollectionId: import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID || '65571b41b12ecb46cb29',
+  postCollectionId: import.meta.env.VITE_APPWRITE_POST_COLLECTION_ID || '65571ae35ef5be4ba8dd',
+  savesCollectionId: import.meta.env.VITE_APPWRITE_SAVES_COLLECTION_ID || '65571b5b163478cdf5db',
+};
+
+export const client = new Client();
+
+client.setEndpoint(appwriteConfig.url);
+client.setProject(appwriteConfig.projectId);
+
+export const account = new Account(client);
+export const databases = new Databases(client);
+export const storage = new Storage(client);
+export const avatars = new Avatars(client);
