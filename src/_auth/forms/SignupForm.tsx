d6282e8c6f088ce85b57ddee36697556fc2ed29c
Harsha Vardhan <94213293+harsha42703@users.noreply.github.com>
2023-11-17 17:23:36 +0530

    Update SignupForm.tsx

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -75,7 +75,7 @@ const SignupForm = () => {
   return (
     <Form {...form}>
       <div className="sm:w-420 flex-center flex-col">
-        <img className="w-[170px] h-[50px]" src="/assets/images/logopng.png" alt="logo" />
+        <img className="w-[180px] h-[50px]" src="/assets/images/logopng.png" alt="logo" />
 
         <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
           Create a new account
